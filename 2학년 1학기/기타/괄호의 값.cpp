c6ad58fdf22d7280655b20c41ddabef136c02f20
Hyeon <powergee101@gmail.com>
2020-06-27 22:17:28 +0900

    Update the solution of BOJ2504

--- 2학년 1학기/기타/괄호의 값.cpp
+++ 2학년 1학기/기타/괄호의 값.cpp
@@ -2,23 +2,53 @@
 #include <string>
 #include <stack>
 
-int getScore(std::string& input)
+bool isValid(std::string& input)
 {
-    std::stack<std::pair<char, int>> parStack;
-    std::stack<int> valStack;
+    std::stack<char> parStack;
 
-    for (int i = 0; i < input.size(); ++i)
+    for (int i = 0; i < (int)input.size(); ++i)
     {
         if (input[i] == '(' || input[i] == '[')
-            parStack.push({input[i], i});
+            parStack.push(input[i]);
         else if (input[i] == ')')
         {
-            if (parStack.empty())
+            if (parStack.empty() || parStack.top() == '[')
                 return false;
-
-            
+            parStack.pop();
+        }
+        else
+        {
+            if (parStack.empty() || parStack.top() == '(')
+                return false;
+            parStack.pop();
         }
     }
+
+    return parStack.size() == 0;
+}
+
+int getScore(std::string& input, int& pos)
+{
+    char opening = input[pos++];
+    char closing = (opening == '(' ? ')' : ']');
+    int childSum = 0;
+
+    while (pos < (int)input.size())
+    {
+        if (input[pos] == closing)
+        {
+            if (childSum == 0)
+                return (opening == '(' ? 2 : 3);
+            else
+                return childSum * (opening == '(' ? 2 : 3);
+        }
+        else
+            childSum += getScore(input, pos); 
+
+        ++pos;
+    }
+
+    return 0;
 }
 
 int main()
@@ -26,5 +56,15 @@ int main()
     std::string input;
     std::cin >> input;
 
-
+    if (isValid(input))
+    {
+        int pos = 0, answer = 0;
+        while (pos < (int)input.size())
+        {
+            answer += getScore(input, pos);
+            ++pos;
+        }
+        printf("%d", answer);
+    }
+    else printf("0");
 }

5076f982b8ac1ef1d37e555a644ebe7451e00617
Hyeon <powergee101@gmail.com>
2020-07-06 01:40:16 +0900

    Update the solution of BOJ17383

--- 2학년 여름방학/기타/옥토끼는 통신교육을 풀어라!!.cpp
+++ 2학년 여름방학/기타/옥토끼는 통신교육을 풀어라!!.cpp
@@ -1,26 +1,71 @@
 #include <iostream>
+#include <set>
+#include <algorithm>
 
 typedef long long Long;
 int n;
-int jobs[100000];
+Long jobs[100000];
+
+inline int toBlock(int jobIndex, Long interval)
+{
+    return jobs[jobIndex] / interval + (jobs[jobIndex] % interval > 0 ? 1 : 0);
+}
 
 bool ableToSchedule(Long interval)
 {
-
+    int left = 0, right = n - 1;
+    while (left <= right)
+    {
+        if (right - left == n-1)
+        {
+            int firstTask = toBlock(left++, interval);
+            if (firstTask != 1)
+                return false;
+        }
+        else if (right == left)
+        {
+            if (toBlock(left++, interval) <= 2)
+                return true;
+            else return false;
+        }
+        else
+        {
+            int longest = toBlock(right--, interval);
+            for (int i = 0; i < longest - 2; ++i)
+            {
+                int taskForOther = toBlock(left++, interval);
+                if (taskForOther != 1)
+                    return false;
+            }
+        }
+    }
+    return true;
 }
 
 Long searchMinInterval(Long left, Long right)
 {
+    if (left == right)
+        return left;
 
+    Long mid = (left + right) / 2;
+    if (ableToSchedule(mid))
+        return searchMinInterval(left, mid);
+    else
+        return searchMinInterval(mid + 1, right);
 }
 
 int main()
 {
     scanf("%d", &n);
+    Long max = 0;
     for (int i = 0; i < n; ++i)
-        scanf("%d", jobs + i);
+    {
+        scanf("%lld", jobs + i);
+        max = std::max(max, jobs[i]);
+    }
+    std::sort(jobs, jobs + n);
 
-    std::cout << searchMinInterval(0, 1e12);
+    std::cout << searchMinInterval(1, max);
 
     return 0;
 }

99740585e12ac49b4117eb5781beb721b466549c
Hyeon <powergee101@gmail.com>
2020-07-10 00:41:20 +0900

    Update the solution of BOJ1212

--- 2학년 여름방학/기타/8진수 2진수.cpp
+++ 2학년 여름방학/기타/8진수 2진수.cpp
@@ -3,21 +3,61 @@
 
 void printBinary(char oc, bool includeZero)
 {
-    if (oc == '0')
-        std::cout << "0";
-
-    else
+    const char* body = "";
+    const char* zeros = "";
+    switch (oc)
     {
-        
+    case '0':
+        zeros = "00";
+        body = "0";
+        break;
+    case '1':
+        zeros = "00";
+        body = "1";
+        break;
+    case '2':
+        zeros = "0";
+        body = "10";
+        break;
+    case '3':
+        zeros = "0";
+        body = "11";
+        break;
+    case '4':
+        zeros = "";
+        body = "100";
+        break;
+    case '5':
+        zeros = "";
+        body = "101";
+        break;
+    case '6':
+        zeros = "";
+        body = "110";
+        break;
+    case '7':
+        zeros = "";
+        body = "111";
+        break;
     }
+
+    if (includeZero)
+        std::cout << zeros;
+    std::cout << body;
 }
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
     std::string input;
     std::cin >> input;
 
-    
+    printBinary(input[0], false);
+    for (int i = 1; i < (int)input.size(); ++i)
+        printBinary(input[i], true);
 
     return 0;
 }

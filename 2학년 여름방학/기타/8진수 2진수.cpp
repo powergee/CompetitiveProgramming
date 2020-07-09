c3d9ba0124e925c7169dc66198bff93cd1fd430f
Hyeon <powergee101@gmail.com>
2020-07-09 23:56:46 +0900

    Add some incomplete solutions

---
+++ 2학년 여름방학/기타/8진수 2진수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+void printBinary(char oc, bool includeZero)
+{
+    if (oc == '0')
+        std::cout << "0";
+
+    else
+    {
+        
+    }
+}
+
+int main()
+{
+    std::string input;
+    std::cin >> input;
+
+    
+
+    return 0;
+}

c48aa05361c9a875b1fc1fa148113976668e3088
Hyeon <powergee101@gmail.com>
2020-08-01 22:46:04 +0900

    Update the Rectangle-free Grid Generator

--- 2학년 여름방학/기타/Rectangle-free Grid/Rectangle-free Grid Generator.cpp
+++ 2학년 여름방학/기타/Rectangle-free Grid/Rectangle-free Grid Generator.cpp
@@ -1,48 +1,96 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <set>
+#include <fstream>
 #include <string>
+#include <vector>
 
-const std::string PATH = "G:\\CompetitiveProgramming\\2학년 여름방학\\기타\\Rectangle-free Grid\\outputs\\";
+const std::string PATH = "/home/hyeon/CompetitiveProgramming/2학년 여름방학/기타/Rectangle-free Grid/outputs/";
+char grid[150][151];
+int n = 150;
+int count = 0;
 
-struct Point
+bool isAbleToMark(int r, int c)
 {
-    int row, col;
+    if (grid[r][c] == 'O')
+        return false;
 
-    bool operator<(const Point& p) const
+    for (int i = 0; i < n; ++i)
     {
-        if (row == p.row)
-            return col < p.col;
-        return row < p.row;
+        for (int j = 0; j < n; ++j)
+        {
+            if (i == r || j == c)
+                continue;
+            
+            if (grid[i][j] == 'O' &&
+                grid[i][c] == 'O' &&
+                grid[r][j] == 'O')
+                return false;
+        }
     }
 
-    bool operator==(const Point& p) const
-    {
-        return row == p.row && col == p.col;
-    }
-};
+    return true;
+}
 
-struct Square
+void fillLayer(int layer)
 {
-    Point p1, p2;
-
-    bool operator<(const Square& s) const
+    for (int i = layer; i >= 0; --i)
     {
-        if (p1 == s.p1)
-            return p2 < s.p2;
-        return p1 < s.p1;
+        // left up
+        if (isAbleToMark(i, layer))
+        {
+            grid[i][layer] = 'O';
+            ++count;
+        }
+        else
+            grid[i][layer] = '.';
+        
+        if (isAbleToMark(layer, i))
+        {
+            grid[layer][i] = 'O';
+            ++count;
+        }
+        else
+            grid[layer][i] = '.';
+        /*
+        // left down
+        if (isAbleToMark(n-1-i, layer))
+            grid[n-1-i][layer] = 'O', ++count;
+        else
+            grid[n-1-i][layer] = '.';
+        
+        if (isAbleToMark(n-1-layer, i))
+            grid[n-1-layer][i] = 'O', ++count;
+        else
+            grid[n-1-layer][i] = '.';
+
+        // right up
+        if (isAbleToMark(i, n-1-layer))
+            grid[i][n-1-layer] = 'O', ++count;
+        else
+            grid[i][n-1-layer] = '.';
+        
+        if (isAbleToMark(layer, n-1-i))
+            grid[layer][n-1-i] = 'O', ++count;
+        else
+            grid[layer][n-1-i] = '.';*/
     }
-};
+}
+
+void dump(int size)
+{
+    std::ofstream output(PATH + std::to_string(size) + ".txt");
+    output << size << std::endl;
+    for (int i = 0; i < size; ++i)
+        output << grid[i] << std::endl;
+    output.close();
+}
 
 int main()
 {
-    char grid[150][151];
-    std::fill(&grid[0][0], &grid[0][0] + sizeof(grid) / sizeof(char), 'O');
-    
-    int n;
-    for (n = 2; n <= 150; ++n)
+    grid[0][0] = '.';
+    for (int layer = 1; layer < n; ++layer)
     {
-        
+        fillLayer(layer);
+        dump(layer+1);
+        printf("%d\n", count);
     }
 }

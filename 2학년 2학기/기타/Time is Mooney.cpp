45214529d09d3fed0769d26284f08ee5f843e211
Jeonghyeon Kim <powergee101@gmail.com>
2020-10-27 13:48:26 +0000

    Add a solution of Time is Mooney (USACO)

--- 2학년 2학기/기타/Time is Mooney.cpp
+++ 2학년 2학기/기타/Time is Mooney.cpp
@@ -1,96 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <climits>
-#include <cmath>
+#include <vector>
 
+typedef long long Long;
 int n, m, c;
-
-struct TripResult
-{
-    bool arrived;
-    int earned, elapsed;
-
-    TripResult()
-    {
-        arrived = false;
-        earned = elapsed = 0;
-    }
-
-    TripResult(bool arrived, int earned, int elapsed)
-    {
-        this->arrived = arrived;
-        this->earned = earned;
-        this->elapsed = elapsed;
-    }
-
-    int calcMooney() const
-    {
-        return earned - c * std::pow(elapsed, 2);
-    }
-
-    bool operator<(const TripResult &t) const
-    {
-        return calcMooney() < t.calcMooney();
-    }
-
-    bool operator!=(const TripResult &t) const
-    {
-        return earned == t.earned && elapsed == t.elapsed;
-    }
-};
-
-const TripResult DEFAULT = TripResult(false, -1, -1);
-const TripResult NO_VALID_ANSWERS = TripResult(false, 0, 0);
-
-int income[1000];
-std::vector<int> graph[1000];
-TripResult dp[1000][1000];
-
-TripResult startTrip(int start, int time)
-{
-    if (time >= 1000)
-        return NO_VALID_ANSWERS;
-
-    TripResult &result = dp[start][time];
-    if (result != DEFAULT)
-        return result;
-
-    result = NO_VALID_ANSWERS;
-    int validNext = -1;
-    for (int next : graph[start])
-    {
-        TripResult subResult = startTrip(next, time + 1);
-        if (subResult.arrived && result < subResult)
-        {
-            validNext = next;
-            result = subResult;
-        }
-    }
-
-    if (start == 1)
-        return result = TripResult(true, result.earned, result.elapsed);
-    else if (validNext == -1)
-        return result = NO_VALID_ANSWERS;
-    else
-        return result = TripResult(true, result.earned + income[validNext], result.elapsed + time);
-}
+Long income[1001];
+// dp[s][t]: t시간에 s라는 정점에 있을 때 가능한 최대 수익
+Long dp[1001][1001];
+std::vector<std::pair<int, int>> edges;
 
 int main()
 {
     scanf("%d %d %d", &n, &m, &c);
-    for (int i = 0; i < n; ++i)
-        scanf("%d", income + i);
 
+    for (int i = 1; i <= n; ++i)
+        scanf("%lld", income+i);
+    
     for (int i = 0; i < m; ++i)
     {
         int u, v;
         scanf("%d %d", &u, &v);
-        graph[u - 1].push_back(v - 1);
+        edges.emplace_back(u, v);
     }
 
-    std::fill(&dp[0][0], &dp[0][0] + sizeof(dp) / sizeof(TripResult), DEFAULT);
-    printf("%d", startTrip(1, 0).calcMooney());
+    Long answer = 0;
+    for (int i = 1; i <= 1000; ++i)
+        dp[0][i] = -1;
+    dp[0][1] = 0;
+
+    for (int t = 1; t <= 1000; ++t)
+    {
+        for (int i = 1; i <= 1000; ++i)
+            dp[t][i] = -1;
+        for (std::pair<int, int>& edge : edges)
+        {
+            int u = edge.first;
+            int v = edge.second;
+            if (dp[t-1][u] != -1)
+                dp[t][v] = std::max(dp[t][v], dp[t-1][u] + income[v]);
+        }
+
+        answer = std::max(answer, dp[t][1] - (Long)c*t*t);
+    }
+
+    printf("%lld", answer);
 
     return 0;
 }

e5ba20174cce3a8fe5af5216bd7479e87a0a5061
Jeonghyeon Kim <powergee101@gmail.com>
2021-05-15 02:43:14 +0900

    Add a solution of 홀짝 칵테일 (BOJ 21312)

---
+++ 3학년 1학기/기타/홀짝 칵테일/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+bool hasOdd = false;
+int oddMul = 1;
+int totalMul = 1;
+foreach (var n in nums) {
+    if (n % 2 != 0) {
+        hasOdd = true;
+        oddMul *= n;
+    }
+    totalMul *= n;
+}
+
+if (hasOdd) {
+    Console.WriteLine(oddMul);
+} else {
+    Console.WriteLine(totalMul);
+}

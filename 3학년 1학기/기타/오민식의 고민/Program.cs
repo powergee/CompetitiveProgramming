8d11f2a60abb069536818b399d8f9c67f13ea231
Jeonghyeon Kim <powergee101@gmail.com>
2021-05-15 02:16:22 +0900

    Add some solutions of Bellman-Ford problems

---
+++ 3학년 1학기/기타/오민식의 고민/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace 오민식의_고민
+{
+    class Edge {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public long Cost { get; set; }
+    }
+
+    class Program
+    {
+        static void DFS(List<Edge> graph, bool[] visited, int start) {
+            visited[start] = true;
+            foreach (Edge e in graph) {
+                if (e.Start == start && !visited[e.End]) {
+                    DFS(graph, visited, e.End);
+                }
+            }
+        }
+
+        static (bool, long?) FindDists(List<Edge> graph, int[] benefits, int n, int start, int end) {
+            long?[] dist = new long?[n];
+            Array.Fill(dist, null);
+            dist[start] = -benefits[start];
+            
+            for (int i = 0; i < n-1; ++i) {
+                foreach (Edge e in graph) {
+                    if (dist[e.Start] != null && (dist[e.End] == null || dist[e.Start] + e.Cost - benefits[e.End] < dist[e.End])) {
+                        dist[e.End] = dist[e.Start] + e.Cost - benefits[e.End];
+                    }
+                }
+            }
+
+            bool[] visited = new bool[n];
+            Array.Fill(visited, false);
+            foreach (Edge e in graph) {
+                if (dist[e.Start] != null && (dist[e.End] == null || dist[e.Start] + e.Cost - benefits[e.End] < dist[e.End])) {
+                    DFS(graph, visited, e.End);
+                }
+            }
+
+            if (dist[end] == null) {
+                return (true, null);
+            } else if (visited[end]) {
+                return (false, null);
+            } else {
+                return (true, -dist[end]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var (n, start, end, m) = (tokens[0], tokens[1], tokens[2], tokens[3]);
+
+            List<Edge> graph = new List<Edge>();
+
+            for (int i = 0; i < m; ++i) {
+                tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var (s, e, c) = (tokens[0], tokens[1], tokens[2]);
+                graph.Add(new Edge() { Start = s, End = e, Cost = c });
+            }
+
+            int[] benefits = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var (hasAnswer, dist) = FindDists(graph, benefits, n, start, end);
+
+            using (StreamWriter writer = new StreamWriter(Console.OpenStandardOutput())) {
+                if (hasAnswer) {
+                    if (dist == null) {
+                        writer.Write("gg\n");
+                    } else {
+                        writer.Write($"{dist}\n");
+                    }
+                } else {
+                    writer.Write("Gee\n");
+                }
+            }
+        }
+    }
+}
---
+++ 3학년 1학기/기타/타임머신/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Solution {
+    class Edge {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public long Cost { get; set; }
+    }
+
+    class Program {
+        static (bool, long?[]) findDists(List<Edge> graph, int n, int start) {
+            long?[] dist = new long?[n];
+            Array.Fill(dist, null);
+            dist[start] = 0;
+            
+            for (int i = 0; i < n-1; ++i) {
+                foreach (Edge e in graph) {
+                    if (dist[e.Start] != null && (dist[e.End] == null || dist[e.Start] + e.Cost < dist[e.End])) {
+                        dist[e.End] = dist[e.Start] + e.Cost;
+                    }
+                }
+            }
+
+            bool hasNegCycle = false;
+            foreach (Edge e in graph) {
+                if (dist[e.Start] != null && (dist[e.End] == null || dist[e.Start] + e.Cost < dist[e.End])) {
+                    hasNegCycle = true;
+                }
+            }
+
+            if (hasNegCycle) {
+                return (false, null);
+            } else {
+                return (true, dist);
+            }
+        }
+
+        static void Main() {
+            int n, m;
+            var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            (n, m) = (tokens[0], tokens[1]);
+
+            List<Edge> graph = new List<Edge>();
+            for (int i = 0; i < m; ++i) {
+                Edge e = new Edge();
+                tokens = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                (e.Start, e.End, e.Cost) = (tokens[0]-1, tokens[1]-1, tokens[2]);
+                graph.Add(e);
+            }
+
+            var (hasAnswer, dist) = findDists(graph, n, 0);
+
+            using (StreamWriter writer = new StreamWriter(Console.OpenStandardOutput())) {
+                if (hasAnswer) {
+                    for (int i = 1; i < n; ++i) {
+                        if (dist[i] == null) {
+                            writer.Write("-1\n");
+                        } else {
+                            writer.Write($"{dist[i]}\n");
+                        }
+                    }
+                } else {
+                    writer.Write("-1\n");
+                }
+            }
+        }
+    }
+}

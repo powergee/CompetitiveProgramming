1e714586f9288cab8803ab37b43d45c53bdcc408
Jeonghyeon Kim <powergee101@gmail.com>
2021-09-24 23:59:43 +0900

    Add some solutions of LCP

---
+++ 3학년 2학기/ICPC 스터디/Suffix Array/최장 공통 부분 문자열.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+class ManberMyers {
+private:
+    int n;
+    std::string str;
+    std::vector<int> sa;
+    std::vector<int> group;
+    std::vector<int> lcp;
+
+    void initSuffixArray() {
+        int n = str.size();
+        std::iota(sa.begin(), sa.end(), 0);
+        std::transform(sa.begin(), sa.end(), group.begin(), [&](int i) -> int {
+            return (int)str[i];
+        });
+
+        for (int d = 1; ; d <<= 1) {
+            auto compareSuffix = [&](int s1, int s2) -> bool {
+                if (group[s1] != group[s2]) {
+                    return group[s1] < group[s2];
+                } else if (s1+d < n && s2+d < n) {
+                    return group[s1+d] < group[s2+d];
+                } else {
+                    return s1 > s2;
+                }
+            };
+            std::sort(sa.begin(), sa.end(), compareSuffix);
+
+            std::vector<int> newGroup(n, 0);
+            for (int i = 0; i < n-1; ++i) {
+                newGroup[i+1] = newGroup[i] + (compareSuffix(sa[i], sa[i+1]) ? 1 : 0);
+            }
+            for (int i = 0; i < n; ++i) {
+                group[sa[i]] = newGroup[i];
+            }
+
+            if (newGroup.back() == n-1) {
+                break;
+            }
+        }
+    }
+
+    void initLCP() {
+        for (int i = 0, k = 0; i < n; ++i, k = std::max(k-1, 0)) {
+            if (group[i] != n-1) {
+                for (int j = sa[group[i]+1]; str[i+k] == str[j+k]; ++k);
+                lcp[group[i]] = k;
+            }
+        }
+    }
+
+public:
+    ManberMyers(std::string str) : str(str), n(str.size()) {
+        sa.resize(n);
+        group.resize(n);
+        lcp.resize(n-1);
+        initSuffixArray();
+        initLCP();
+    }
+
+    const std::vector<int>& getSuffixArray() const {
+        return sa;
+    }
+
+    const std::vector<int>& getLCP() const {
+        return lcp;
+    }
+
+    int getLength() const {
+        return n;
+    }
+};
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::string a, b;
+    std::cin >> a >> b;
+    std::string comb = a + "$" + b;
+    ManberMyers manber(comb);
+
+    auto& sa = manber.getSuffixArray();
+    auto& lcp = manber.getLCP();
+    
+    int maxLength = -1;
+    int maxIdx = -1;
+    for (int i = 1; i < manber.getLength()-1; ++i) {
+        bool prev = (sa[i] < a.size() ? true : false);
+        bool next = (sa[i+1] < a.size() ? true : false);
+        if (prev != next && maxLength < lcp[i]) {
+            maxLength = lcp[i];
+            maxIdx = i;
+        }
+    }
+
+    std::cout << maxLength << "\n";
+    int s1 = sa[maxIdx], s2 = sa[maxIdx+1];
+    while (s1 < comb.size() && s2 < comb.size() && comb[s1] == comb[s2]) {
+        std::cout << comb[s1];
+        ++s1; ++s2;
+    }
+
+    return 0;
+}

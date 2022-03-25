358172b8d6e66ef199e28eebb1f8eab6cc32fba3
Jeonghyeon Kim <powergee101@gmail.com>
2022-03-26 02:03:56 +0900

    Update Aho-Corasick: Move AC-Related functions to Trie

--- 알고리즘 기본 구현/Aho-Corasick.cpp
+++ 알고리즘 기본 구현/Aho-Corasick.cpp
@@ -37,64 +37,64 @@ struct Trie {
         }
         next[nextIdx]->insert(str, start + 1);
     }
+
+    void buildFail() {
+        std::queue<Trie *> q;
+        this->fail = this;
+        q.push(this);
+
+        while (!q.empty()) {
+            Trie *current = q.front();
+            q.pop();
+
+            for (int i = 0; i < 26; i++) {
+                Trie *next = current->next[i];
+
+                if (!next) {
+                    continue;
+                } else if (current == this) {
+                    next->fail = this;
+                } else {
+                    Trie *dest = current->fail;
+                    while (dest != this && !dest->next[i]) {
+                        dest = dest->fail;
+                    }
+                    if (dest->next[i]) {
+                        dest = dest->next[i];
+                    }
+                    next->fail = dest;
+                }
+
+                // 실제 매칭된 문자열이 필요하다면 아래 정의 사용
+                // if (next->fail->outputs.size() > 0) {
+                //     next->outputs.insert(next->outputs.end(), current->outputs.begin(), current->outputs.end());
+                // }
+                // 매칭 여부만 필요하다면
+                if (next->fail->matched) {
+                    next->matched = true;
+                }
+                q.push(next);
+            }
+        }
+    }
+
+    bool find(std::string &query) {
+        Trie *current = this;
+        bool result = false;
+
+        for (int c = 0; c < (int)query.size(); c++) {
+            int nextIdx = query[c] - 'a';
+            while (current != this && !current->next[nextIdx]) {
+                current = current->fail;
+            }
+            if (current->next[nextIdx]) {
+                current = current->next[nextIdx];
+            }
+            if (current->matched) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 };
-
-void buildFail(Trie *root) {
-    std::queue<Trie *> q;
-    root->fail = root;
-    q.push(root);
-
-    while (!q.empty()) {
-        Trie *current = q.front();
-        q.pop();
-
-        for (int i = 0; i < 26; i++) {
-            Trie *next = current->next[i];
-
-            if (!next) {
-                continue;
-            } else if (current == root) {
-                next->fail = root;
-            } else {
-                Trie *dest = current->fail;
-                while (dest != root && !dest->next[i]) {
-                    dest = dest->fail;
-                }
-                if (dest->next[i]) {
-                    dest = dest->next[i];
-                }
-                next->fail = dest;
-            }
-
-            // 실제 매칭된 문자열이 필요하다면 아래 정의 사용
-            // if (next->fail->outputs.size() > 0) {
-            //     next->outputs.insert(next->outputs.end(), current->outputs.begin(), current->outputs.end());
-            // }
-            // 매칭 여부만 필요하다면
-            if (next->fail->matched) {
-                next->matched = true;
-            }
-            q.push(next);
-        }
-    }
-}
-
-bool find(Trie *root, std::string &query) {
-    Trie *current = root;
-    bool result = false;
-
-    for (int c = 0; c < (int)query.size(); c++) {
-        int nextIdx = query[c] - 'a';
-        while (current != root && !current->next[nextIdx]) {
-            current = current->fail;
-        }
-        if (current->next[nextIdx]) {
-            current = current->next[nextIdx];
-        }
-        if (current->matched) {
-            result = true;
-            break;
-        }
-    }
-    return result;
-}

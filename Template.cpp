fd3ae19bcaabce89b767418370122c5d3ad0ed09
Jeonghyeon Kim <powergee101@gmail.com>
2022-06-02 02:59:02 +0900

    Replace "write" with "print" / "read" with "scan"

--- Template.cpp
+++ Template.cpp
@@ -17,88 +17,94 @@ struct IO {
     }
 
     template<typename T>
-    T read() {
+    T scan() {
         T result;
         std::cin >> result;
         return result;
     }
 
-    int readInt() { return read<int>(); }
-    Long readLong() { return read<Long>(); }
-    double readDouble() { return read<double>(); }
-    std::string readToken() { return read<std::string>(); }
+    int scanInt() { return scan<int>(); }
+    Long scanLong() { return scan<Long>(); }
+    double scanDouble() { return scan<double>(); }
+    std::string scanToken() { return scan<std::string>(); }
 
     template<typename T>
-    std::vector<T> reads(int n) {
+    std::vector<T> scans(int n) {
         std::vector<T> arr(n);
         for (int i = 0; i < n; ++i) {
-            arr[i] = read<T>();
+            arr[i] = scan<T>();
         }
         return arr;
     }
 
-    std::vector<int> readInts(int n) { return reads<int>(n); }
-    std::vector<Long> readLongs(int n) { return reads<Long>(n); }
-    std::vector<double> readDoubles(int n) { return reads<double>(n); }
-    std::vector<std::string> readTokens(int n) { return reads<std::string>(n); }
+    std::vector<int> scanInts(int n) { return scans<int>(n); }
+    std::vector<Long> scanLongs(int n) { return scans<Long>(n); }
+    std::vector<double> scanDoubles(int n) { return scans<double>(n); }
+    std::vector<std::string> scanTokens(int n) { return scans<std::string>(n); }
 
     template<typename T, unsigned int N>
-    std::array<T, N> reads() {
+    std::array<T, N> scans() {
         std::array<T, N> result;
         for (int i = 0; i < N; ++i) {
-            result[i] = readInt();
+            result[i] = scanInt();
         }
         return result;
     }
 
-    template<unsigned int N> std::array<int, N> readInts() { return reads<int, N>(); }
-    template<unsigned int N> std::array<Long, N> readLongs() { return reads<Long, N>(); }
-    template<unsigned int N> std::array<double, N> readDoubles() { return reads<double, N>(); }
-    template<unsigned int N> std::array<std::string, N> readTokens() { return reads<std::string, N>(); }
+    template<unsigned int N> std::array<int, N> scanInts() { return scans<int, N>(); }
+    template<unsigned int N> std::array<Long, N> scanLongs() { return scans<Long, N>(); }
+    template<unsigned int N> std::array<double, N> scanDoubles() { return scans<double, N>(); }
+    template<unsigned int N> std::array<std::string, N> scanTokens() { return scans<std::string, N>(); }
 
-    std::vector<std::vector<int>> readMatrix(int r, int c) {
+    std::vector<std::vector<int>> scanMatrix(int r, int c) {
         std::vector<std::vector<int>> result(r, std::vector<int>(c));
         for (int i = 0; i < r; ++i) {
-            result[i] = readInts(c);
+            result[i] = scanInts(c);
         }
         return result;
     }
 
     template<typename T>
-    void write(T value) {
+    void print(T value) {
         std::cout << value << " ";
     }
 
     template<typename T, typename... Args>
-    void write(T value, Args... args) {
+    void print(T value, Args... args) {
         std::cout << value << " ";
-        write(args...);
+        print(args...);
     }
 
-    void writeLine() {
+    void printLine() {
         std::cout << "\n";
     }
 
     template<typename T>
-    void writeLine(T value) {
+    void printLine(T value) {
         std::cout << value << "\n";
     }
 
     template<typename T, typename... Args>
-    void writeLine(T value, Args... args) {
+    void printLine(T value, Args... args) {
         std::cout << value << " ";
-        writeLine(args...);
+        printLine(args...);
+    }
+
+    void untilEOF(std::function<void()> solve) {
+        while (std::cin.eof() != false) {
+            solve();
+        }
     }
 
     void codeforces(std::function<void()> solve) {
-        int T = readInt();
+        int T = scanInt();
         while (T--) {
             solve();
         }
     }
 
     void google(std::function<void()> solve) {
-        int T = readInt();
+        int T = scanInt();
         for (int t = 1; t <= T; ++t) {
             std::cout << "Case #" << t << ": ";
             solve();

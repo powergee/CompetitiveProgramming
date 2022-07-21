034c06739195cbc680387bfc69dcfecbef7f7a62
Jeonghyeon Kim <powergee101@gmail.com>
2022-07-22 02:44:20 +0900

    Add solutions(A~D) of CF Edu 132

---
+++ 코드포스/Edu 132/D.cpp
@@ -0,0 +1,357 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <functional>
+#include <numeric>
+#include <array>
+#include <queue>
+#include <map>
+#include <set>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using OrderedSet = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using Long = long long;
+using BigInt = __int128_t;
+
+template<typename T>
+struct IsContainer {
+    typedef typename std::remove_const<T>::type PlainType;
+
+    template<typename A>
+    static constexpr bool test(
+        A * pt,
+        A const * cpt = nullptr,
+        decltype(pt->begin()) * = nullptr,
+        decltype(pt->end()) * = nullptr,
+        decltype(cpt->begin()) * = nullptr,
+        decltype(cpt->end()) * = nullptr,
+        typename A::iterator * pi = nullptr,
+        typename A::const_iterator * pci = nullptr) {
+
+        typedef typename A::iterator iterator;
+        typedef typename A::const_iterator const_iterator;
+        return std::is_same<decltype(pt->begin()), iterator>::value &&
+            std::is_same<decltype(pt->end()), iterator>::value &&
+            std::is_same<decltype(cpt->begin()), const_iterator>::value &&
+            std::is_same<decltype(cpt->end()), const_iterator>::value;
+    }
+
+    template<typename A>
+    static constexpr bool test(...) { return false; }
+
+    static constexpr bool value = test<PlainType>(nullptr);
+};
+
+template <size_t Dim, typename T>
+struct TensorTemp {
+    typedef std::vector<typename TensorTemp<Dim-1, T>::type> type;
+};
+
+template<typename T>
+struct TensorTemp<1, T>  {
+    typedef T type;
+};
+
+template <size_t Dim, typename T>
+using Tensor = std::vector<typename TensorTemp<Dim, T>::type>;
+
+template<typename T, typename FirstDim, typename... RestDims>
+Tensor<sizeof...(RestDims)+1, T> createTensor(T init, FirstDim firstDim, RestDims... restDims) {
+    constexpr size_t DimCount = sizeof...(restDims)+1;
+    if constexpr (DimCount > 1) {
+        return Tensor<DimCount, T>(firstDim, createTensor<T>(init, restDims...));
+    } else {
+        return Tensor<DimCount, T>(firstDim, init);
+    }
+}
+
+template<
+    typename T,
+    typename = std::enable_if_t<IsContainer<T>::value>,
+    typename = std::enable_if_t<!std::is_same<T, std::string>::value>>
+std::ostream& operator<<(std::ostream& os, const T &cont) {
+    for (auto it = cont.begin(); it != cont.end(); ++it) {
+        std::cout << (it == cont.begin() ? "" : " ") << *it;
+    }
+    return os;
+}
+
+template<
+    typename ContType,
+    typename MapFunc,
+    typename = std::enable_if_t<IsContainer<ContType>::value>,
+    typename ElemType = typename ContType::value_type>
+auto map(const ContType& cont, const MapFunc& func) {
+    typedef decltype(func(*cont.begin())) RetType;
+    std::vector<RetType> result;
+    for (auto v : cont) {
+        result.push_back(func(v));
+    }
+    return result;
+}
+
+template<
+    typename ContType,
+    typename = std::enable_if_t<IsContainer<ContType>::value>,
+    typename ElemType = typename ContType::value_type>
+std::map<ElemType, int> countElements(const ContType& cont) {
+    std::map<ElemType, int> count;
+    for (auto v : cont) {
+        count[v]++;
+    }
+    return count;
+}
+
+template<typename... Ts>
+std::ostream& operator<<(std::ostream& os, std::pair<Ts...> const &p) {
+    std::cout << p.first << " " << p.second;
+    return os;
+}
+
+struct IO {
+    IO() {
+        std::cin.tie(nullptr)->sync_with_stdio(false);
+    }
+
+    template<typename T>
+    T next() {
+        T result;
+        std::cin >> result;
+        return result;
+    }
+
+    int nextInt() { return next<int>(); }
+    Long nextLong() { return next<Long>(); }
+    double nextDouble() { return next<double>(); }
+    std::string nextToken() { return next<std::string>(); }
+
+    template<typename T>
+    std::vector<T> nexts(int n, int leftPadding = 0) {
+        std::vector<T> arr(leftPadding + n);
+        for (int i = leftPadding; i < arr.size(); ++i) {
+            arr[i] = next<T>();
+        }
+        return arr;
+    }
+
+    std::vector<int> nextInts(int n, int leftPadding = 0) { return nexts<int>(n, leftPadding); }
+    std::vector<Long> nextLongs(int n, int leftPadding = 0) { return nexts<Long>(n, leftPadding); }
+    std::vector<double> nextDoubles(int n, int leftPadding = 0) { return nexts<double>(n, leftPadding); }
+    std::vector<std::string> nextTokens(int n, int leftPadding = 0) { return nexts<std::string>(n, leftPadding); }
+
+    template<typename T, unsigned int N>
+    std::array<T, N> nexts() {
+        std::array<T, N> result;
+        for (int i = 0; i < N; ++i) {
+            result[i] = next<T>();
+        }
+        return result;
+    }
+
+    template<unsigned int N> std::array<int, N> nextInts() { return nexts<int, N>(); }
+    template<unsigned int N> std::array<Long, N> nextLongs() { return nexts<Long, N>(); }
+    template<unsigned int N> std::array<double, N> nextDoubles() { return nexts<double, N>(); }
+    template<unsigned int N> std::array<std::string, N> nextTokens() { return nexts<std::string, N>(); }
+
+    Tensor<2, int> nextMatrix(int r, int c) {
+        Tensor<2, int> result(r, std::vector<int>(c));
+        for (int i = 0; i < r; ++i) {
+            result[i] = nextInts(c);
+        }
+        return result;
+    }
+
+    Tensor<2, int> nextTree(int n) {
+        Tensor<2, int> result(n+1);
+        for (int i = 0; i < n-1; ++i) {
+            auto [u, v] = nexts<int, 2>();
+            result[u].push_back(v);
+            result[v].push_back(u);
+        }
+        return result;
+    }
+
+    template<typename T>
+    void print(const T& value) {
+        std::cout << value << " ";
+    }
+
+    template<typename T, typename... Args>
+    void print(const T& value, Args... args) {
+        print(value);
+        print(args...);
+    }
+
+    void println() {
+        std::cout << "\n";
+    }
+
+    template<typename T>
+    void println(const T& value) {
+        std::cout << value << "\n";
+    }
+
+    template<typename T, typename... Args>
+    void println(const T& value, Args... args) {
+        std::cout << value << " ";
+        println(args...);
+    }
+
+    void printYes(bool yes) {
+        println(yes ? "Yes" : "No");
+    }
+
+    void untilEOF(std::function<void()> solve) {
+        while (std::cin.eof() != false) {
+            solve();
+        }
+    }
+
+    void codeforces(std::function<void()> solve) {
+        int T = nextInt();
+        while (T--) {
+            solve();
+        }
+    }
+
+    void google(std::function<void()> solve) {
+        int T = nextInt();
+        for (int t = 1; t <= T; ++t) {
+            std::cout << "Case #" << t << ": ";
+            solve();
+        }
+    }
+
+    void samsung(std::function<void()> solve) {
+        int T = nextInt();
+        for (int t = 1; t <= T; ++t) {
+            std::cout << "Case #" << t << "\n";
+            solve();
+            // For partial scores:
+            std::cout.flush();
+        }
+    }
+} io;
+
+#include <functional>
+#include <vector>
+#include <cmath>
+
+using Long = long long;
+
+template <typename T>
+class SegTree {
+private:
+    int originCount;
+    std::vector<T> tree;
+    std::function<T(T, T)> reduce;
+    
+    void initialize(int index, int start, int end, const std::vector<T>& original) {
+        if (start == end) {
+            tree[index] = original[start];
+        } else {
+            int mid = (start + end) / 2;
+            initialize(index*2, start, mid, original);
+            initialize(index*2+1, mid+1, end, original);
+            tree[index] = reduce(tree[index*2], tree[index*2+1]);
+        }
+    }
+
+    T query(int index, int reqStart, int reqEnd, int treeStart, int treeEnd) {
+        reqStart = std::max(reqStart, treeStart);
+        reqEnd = std::min(reqEnd, treeEnd);
+        if (reqStart == treeStart && treeEnd == reqEnd) {
+            return tree[index];
+        }
+
+        int treeMid = (treeStart+treeEnd)/2;
+        bool left = !(treeMid < reqStart);
+        bool right = !(reqEnd <= treeMid);
+
+        if (left && right) {
+            return reduce(query(index*2, reqStart, reqEnd, treeStart, treeMid),
+                          query(index*2+1, reqStart, reqEnd, treeMid+1, treeEnd));
+        } else if (left) {
+            return query(index*2, reqStart, reqEnd, treeStart, treeMid);
+        } else {
+            return query(index*2+1, reqStart, reqEnd, treeMid+1, treeEnd);
+        }
+    }
+
+    void update(T add, int index, int reqPos, int treeStart, int treeEnd) {
+        if (treeStart == reqPos && treeEnd == reqPos) {
+            tree[index] += add;
+        } else if (treeStart <= reqPos && reqPos <= treeEnd) {
+            int treeMed = (treeStart + treeEnd) / 2;
+            update(add, index*2, reqPos, treeStart, treeMed);
+            update(add, index*2+1, reqPos, treeMed+1, treeEnd);
+            tree[index] = reduce(tree[index*2], tree[index*2+1]);
+        }
+    }
+
+    int findLowerBound(T sum, int index, int treeStart, int treeEnd) {
+        int treeMid = (treeStart+treeEnd) / 2;
+        if (treeStart == treeEnd) {
+            return treeStart;
+        } else if (tree[index*2] < sum) {
+            return findLowerBound(sum-tree[index*2], index*2+1, treeMid+1, treeEnd);
+        } else {
+            return findLowerBound(sum, index*2, treeStart, treeMid);
+        }
+    }
+
+public:
+    SegTree(int count, std::function<T(T, T)> reduce) : reduce(reduce) {
+        originCount = count;
+        int treeHeight = (int)std::ceil((float)std::log2(originCount));
+        int vecSize = (1 << (treeHeight+1));
+        tree.resize(vecSize);
+    }
+
+    SegTree(const std::vector<T>& original, std::function<T(T, T)> reduce) : SegTree(original.size(), reduce) {
+        initialize(1, 0, originCount-1, original);
+    }
+
+    T query(int start, int end) {
+        return query(1, start, end, 0, originCount-1);
+    }
+
+    void update(int pos, T add) {
+        update(add, 1, pos, 0, originCount-1);
+    }
+
+    int findLowerBound(T sum) {
+        return findLowerBound(sum, 1, 0, originCount-1);
+    }
+};
+
+int main() {
+    auto [n, m] = io.nexts<int, 2>();
+    auto arr = io.nextLongs(m);
+    int q = io.nextInt();
+
+    SegTree<Long> maxTree(arr, [](Long a, Long b) { return std::max(a, b); });
+
+    while (q--) {
+        auto [sx, sy, ex, ey, k] = io.nexts<Long, 5>();
+        io.printYes([&]() -> bool {
+            if (std::abs(sx-ex) % k != 0 || std::abs(sy-ey) % k != 0) {
+                return false;
+            }
+            Long maxHeight = (n-sx)/k*k + sx;
+            Long leftCol = std::min(sy, ey);
+            Long rightCol = std::max(sy, ey);
+            if (maxTree.query(leftCol-1, rightCol-1) >= maxHeight) {
+                return false;
+            }
+            return true;
+        }());
+    }
+    
+    return 0;
+}

8d3b2c64d8bbdcfd8946ad45850721cdfcac120c
Jeonghyeon Kim <powergee101@gmail.com>
2022-09-16 04:03:55 +0900

    Add simple rust playground

---
+++ rust-template/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let scan = &mut Scanner::default();
+    
+}
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}

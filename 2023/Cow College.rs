e46d41a022ed4c568d820d4f0d6b5afedc66d784
Jeonghyeon Kim <powergee101@gmail.com>
2023-02-15 03:28:22 +0900

    Add solutions of USACO 2022 December Bronze

---
+++ 2023/Cow College.rs
@@ -0,0 +1,77 @@
+#![allow(unused)]
+use std::io::{BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
+    let io = &mut IO::new(stdin.lock(), stdout.lock());
+
+    let n = io.usize();
+    let mut arr = io.usizes(n);
+    arr.sort();
+
+    let mut answer = (0, 0);
+    for i in 0..n {
+        let curr = (arr[i] * (n-i), arr[i]);
+        if curr.0 > answer.0 {
+            answer = curr;
+        }
+    }
+
+    writeln!(io.writer, "{} {}", answer.0, answer.1).unwrap();
+}
+
+// Modified EbTech's Scanner
+pub struct IO<R, W: Write> {
+    reader: R,
+    writer: BufWriter<W>,
+    buf_str: String,
+    buf_iter: std::str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead, W: Write> IO<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        Self {
+            reader,
+            writer: BufWriter::new(writer),
+            buf_str: String::new(),
+            buf_iter: "".split_whitespace(),
+        }
+    }
+
+    #[inline]
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_line(&mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe { std::mem::transmute(self.buf_str.split_whitespace()) };
+        }
+    }
+}
+
+macro_rules! io_shortcut {
+    ($scan_type:ident, $single_ident:ident, $multi_ident:ident) => {
+        impl<R: BufRead, W: Write> IO<R, W> {
+            #[inline]
+            #[allow(unused)]
+            fn $single_ident(&mut self) -> $scan_type {
+                self.next()
+            }
+            #[inline]
+            #[allow(unused)]
+            fn $multi_ident(&mut self, n: usize) -> Vec<$scan_type> {
+                (0..n).map(|_| self.next()).collect()
+            }
+        }
+    };
+}
+io_shortcut!(i64, i64, i64s);
+io_shortcut!(u64, u64, u64s);
+io_shortcut!(usize, usize, usizes);
+io_shortcut!(f64, f64, f64s);
+io_shortcut!(String, string, strings);
---
+++ 2023/Feeding the Cows.rs
@@ -0,0 +1,98 @@
+#![allow(unused)]
+use std::io::{BufRead, BufWriter, Write};
+
+fn main() {
+    let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
+    let io = &mut IO::new(stdin.lock(), stdout.lock());
+
+    let tt = io.usize();
+    for _ in 0..tt {
+        let (n, k) = (io.usize(), io.usize());
+        let mut map = io.string().chars().collect::<Vec<_>>();
+        let mut answer = vec!['.'; n];
+        let mut count = 0;
+
+        for i in 0..n {
+            let ch = match map[i] {
+                'G' | 'H' => map[i],
+                _ => continue,
+            };
+
+            count += 1;
+            let pos = (n - 1).min(i + k);
+            if answer[pos] == '.' {
+                answer[pos] = ch;
+            } else {
+                assert!(answer[pos-1] == '.');
+                answer[pos-1] = ch;
+            }
+            
+            for j in i..=(i + 2 * k) {
+                if j >= n {
+                    break;
+                } else if map[j] == ch {
+                    map[j] = '.';
+                }
+            }
+        }
+
+        writeln!(io.writer, "{count}").unwrap();
+        writeln!(io.writer, "{}", answer.iter().collect::<String>()).unwrap()
+    }
+}
+
+// Modified EbTech's Scanner
+pub struct IO<R, W: Write> {
+    reader: R,
+    writer: BufWriter<W>,
+    buf_str: String,
+    buf_iter: std::str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead, W: Write> IO<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        Self {
+            reader,
+            writer: BufWriter::new(writer),
+            buf_str: String::new(),
+            buf_iter: "".split_whitespace(),
+        }
+    }
+
+    #[inline]
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_line(&mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe { std::mem::transmute(self.buf_str.split_whitespace()) };
+        }
+    }
+}
+
+macro_rules! io_shortcut {
+    ($scan_type:ident, $single_ident:ident, $multi_ident:ident) => {
+        impl<R: BufRead, W: Write> IO<R, W> {
+            #[inline]
+            #[allow(unused)]
+            fn $single_ident(&mut self) -> $scan_type {
+                self.next()
+            }
+            #[inline]
+            #[allow(unused)]
+            fn $multi_ident(&mut self, n: usize) -> Vec<$scan_type> {
+                (0..n).map(|_| self.next()).collect()
+            }
+        }
+    };
+}
+io_shortcut!(i64, i64, i64s);
+io_shortcut!(u64, u64, u64s);
+io_shortcut!(usize, usize, usizes);
+io_shortcut!(f64, f64, f64s);
+io_shortcut!(String, string, strings);

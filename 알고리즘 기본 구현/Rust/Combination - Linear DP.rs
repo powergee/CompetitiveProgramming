ab0b29e634812566e45f6281e06b531316345fa7
Jeonghyeon Kim <powergee101@gmail.com>
2023-02-17 02:28:38 +0900

    Update `Combination - Linear DP.rs`

--- 알고리즘 기본 구현/Rust/Combination - Linear DP.rs
+++ 알고리즘 기본 구현/Rust/Combination - Linear DP.rs
@@ -1,59 +1,45 @@
-struct Combination {
-    fact: Vec<Option<u64>>,
-    modulo: u64
+pub struct CombCalc {
+    fact_dp: Vec<usize>,
+    bound: usize,
 }
 
-impl Combination {
-    fn new(max: usize, modulo: u64) -> Combination {
-        Combination {
-            fact: vec![None; max+1],
-            modulo
+impl CombCalc {
+    pub fn new(size: usize, bound: usize) -> Self {
+        let mut fact_dp = Vec::with_capacity(size + 1);
+        fact_dp.push(1);
+        for i in 1..=size {
+            fact_dp.push((*fact_dp.last().unwrap() * i) % bound);
         }
+        Self { fact_dp, bound }
     }
 
-    fn factorial(&mut self, n: usize) -> u64 {
-        if n == 0 {
-            1
-        } else if let Some(result) = self.fact[n] {
-            result
-        } else {
-            let result = (self.factorial(n-1)*(n as u64)) % self.modulo;
-            self.fact[n] = Some(result);
-            result
-        }
-    }
-
-    fn pow_mod(&self, val: u64, exp: u64) -> u64 {
-        if val == 0 {
-            0
-        } else if exp == 0 {
-            1
-        } else {
-            let pow = self.pow_mod(val, exp/2);
-            (pow*pow % self.modulo) * if exp % 2 == 0 { 1 } else { val } % self.modulo
-        }
-    }
-
-    fn mod_inverse(&self, val: u64) -> u64 {
-        self.pow_mod(val, self.modulo-2)
-    }
-
-    fn comb(&mut self, n: usize, k: usize) -> u64 {
+    pub fn comb(&self, n: usize, k: usize) -> usize {
         if n < k {
-            0
-        } else {
-            let num = self.factorial(n);
-            let den = (self.factorial(n-k)*self.factorial(k)) % self.modulo;
-            let den = self.mod_inverse(den);
-            (num * den) % self.modulo
+            return 0;
         }
+        (self.fact_dp[n] * self.mod_inverse((self.fact_dp[n - k] * self.fact_dp[k]) % self.bound))
+            % self.bound
     }
 
-    fn homo_comb(&mut self, n: usize, k: usize) -> u64 {
-        self.comb(n+k-1, n-1)
+    pub fn homo_comb(&self, n: usize, k: usize) -> usize {
+        self.comb(n + k - 1, n - 1)
     }
 
-    fn catalan(&mut self, n: usize) -> u64 {
-        (self.comb(2*n, n) * self.mod_inverse((n+1) as u64)) % self.modulo
+    pub fn catalan(&self, n: usize) -> usize {
+        (self.comb(2 * n, n) * self.mod_inverse(n + 1)) % self.bound
+    }
+
+    fn mod_pow(&self, val: usize, exp: usize) -> usize {
+        if val == 0 {
+            return 0;
+        } else if exp == 0 {
+            return 1;
+        }
+        let pow = self.mod_pow(val, exp / 2);
+        (((pow * pow) % self.bound) * (if exp % 2 == 0 { 1 } else { val })) % self.bound
+    }
+
+    fn mod_inverse(&self, val: usize) -> usize {
+        self.mod_pow(val, self.bound - 2)
     }
 }

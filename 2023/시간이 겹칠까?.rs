2c18330610b987ec52f5e5e3f752173560698b06
Jeonghyeon Kim <powergee101@gmail.com>
2023-05-16 16:50:43 +0900

    Add solutions of 시간이 겹칠까? and 첨탑 밀어서 부수기

---
+++ 2023/시간이 겹칠까?.rs
@@ -0,0 +1,82 @@
+#![allow(unused)]
+use std::io::{stdin, stdout, BufRead, BufWriter, StdinLock, StdoutLock, Write};
+
+const LENGTH: usize = 1000002;
+
+fn main() {
+    let mut io = IO::stdio();
+    let n = io.usize();
+
+    let mut count = vec![0i32; LENGTH];
+    for _ in 0..n {
+        let (s, e) = (io.usize(), io.usize());
+        count[s] += 1;
+        count[e+1] -= 1;
+    }
+
+    for i in 1..LENGTH {
+        count[i] += count[i-1];
+    }
+
+    let q = io.usize();
+    for p in io.usizes(q) {
+        writeln!(io.writer, "{}", count[p]).unwrap();
+    }
+}
+
+// Modified EbTech's Scanner
+pub struct IO<R, W: Write> {
+    reader: R,
+    writer: BufWriter<W>,
+    buf_str: String,
+    buf_iter: std::str::SplitWhitespace<'static>,
+}
+
+impl IO<StdinLock<'static>, StdoutLock<'static>> {
+    pub fn stdio() -> Self {
+        Self::new(stdin().lock(), stdout().lock())
+    }
+}
+
+impl<R: BufRead, W: Write> IO<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        Self {
+            reader,
+            writer: BufWriter::new(writer),
+            buf_str: String::new(),
+            buf_iter: "".split_whitespace(),
+        }
+    }
+
+    #[inline]
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_line(&mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe { std::mem::transmute(self.buf_str.split_whitespace()) };
+        }
+    }
+}
+
+macro_rules! io_shortcut {
+    ($scan_type:ident, $single_ident:ident, $multi_ident:ident) => {
+        impl<R: BufRead, W: Write> IO<R, W> {
+            #[inline]
+            #[allow(unused)]
+            fn $single_ident(&mut self) -> $scan_type {
+                self.next()
+            }
+            #[inline]
+            #[allow(unused)]
+            fn $multi_ident(&mut self, n: usize) -> Vec<$scan_type> {
+                (0..n).map(|_| self.next()).collect()
+            }
+        }
+    };
+}
+io_shortcut!(usize, usize, usizes);

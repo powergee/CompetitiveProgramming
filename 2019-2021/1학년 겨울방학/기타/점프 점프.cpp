29497c76787b419428c39241fa3f2c7126b1f5ee
Jeonghyeon Kim <powergee101@gmail.com>
2023-06-18 22:04:44 +0900

    Update a hacked solution: 점프 점프 (BOJ)

--- 2019-2021/1학년 겨울방학/기타/점프 점프.cpp
+++ 2019-2021/1학년 겨울방학/기타/점프 점프.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 
-int main()
-{
+int main() {
     int X, Y, p1, p2;
     scanf("%d %d %d %d", &X, &Y, &p1, &p2);
-    
-    bool success = false;
-    for (int a = 0; a <= 1<<6; ++a)
-    {
-        if ((X * a + p1 - p2) >= 0 && (X * a + p1 - p2) % Y == 0)
-        {
+
+    for (int a = 0; a <= 10000; ++a) {
+        if ((X * a + p1 - p2) >= 0 && (X * a + p1 - p2) % Y == 0) {
             printf("%d", p1 + a * X);
-            success = true;
-            break;
+            return 0;
         }
     }
 
-    if (!success)
-        printf("-1");
-
+    printf("-1");
     return 0;
 }

c7ae7cd7a5136960a843c64987a83718bba31e20
Jeonghyeon Kim <powergee101@gmail.com>
2023-07-18 00:58:20 +0900

    Add a solution of 과 조사하기 (BOJ)

---
+++ 2023/과 조사하기.rs
@@ -0,0 +1,87 @@
+use std::io::{stdin, stdout, BufRead, BufWriter, StdinLock, StdoutLock, Write};
+
+fn main() {
+    let mut io = IO::stdio();
+    let p = io.usize();
+    let mut s = 0;
+    let mut e = 0;
+    let mut a = 0;
+    let mut j = 0;
+    for _ in 0..p {
+        let (g, c, _) = (io.usize(), io.usize(), io.usize());
+        if g == 1 {
+            j += 1;
+        } else if 1 <= c && c <= 2 {
+            s += 1;
+        } else if c == 3 {
+            e += 1;
+        } else {
+            a += 1;
+        }
+    }
+    writeln!(io.writer, "{s}\n{e}\n{a}\n{j}").unwrap();
+}
+
+/// Modified EbTech's Scanner
+pub struct IO<R, W: Write> {
+    reader: R,
+    writer: BufWriter<W>,
+    buf_str: String,
+    buf_iter: std::str::SplitWhitespace<'static>,
+}
+
+impl IO<StdinLock<'static>, StdoutLock<'static>> {
+    pub fn stdio() -> Self {
+        Self::new(stdin().lock(), stdout().lock())
+    }
+}
+
+impl<R: BufRead, W: Write> IO<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        Self {
+            reader,
+            writer: BufWriter::new(writer),
+            buf_str: String::new(),
+            buf_iter: "".split_whitespace(),
+        }
+    }
+
+    #[inline]
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_line(&mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe { std::mem::transmute(self.buf_str.split_whitespace()) };
+        }
+    }
+}
+
+macro_rules! io_shortcut {(
+    $(($type:ident, $types:ident))*
+) => (
+    $(
+        impl<R: BufRead, W: Write> IO<R, W> {
+            #[inline]
+            #[allow(unused)]
+            fn $type(&mut self) -> $type {
+                self.next()
+            }
+            #[inline]
+            #[allow(unused)]
+            fn $types(&mut self, n: usize) -> Vec<$type> {
+                (0..n).map(|_| self.next()).collect()
+            }
+        }
+    )*
+)}
+
+io_shortcut! {
+    (u8, u8s) (u16, u16s) (u32, u32s) (u64, u64s) (u128, u128s)
+    (i8, i8s) (i16, i16s) (i32, i32s) (i64, i64s) (i128, i128s)
+    (usize, usizes) (isize, isizes) (f64, f64s)
+}

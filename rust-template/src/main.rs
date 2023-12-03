e10524bc06b7cf2c3b24963875c827dff1c70d0d
Jeonghyeon Kim <powergee101@gmail.com>
2023-12-04 00:11:44 +0900

    Simplify Rust IO template

--- rust-template/src/main.rs
+++ rust-template/src/main.rs
@@ -1,70 +1,27 @@
 #![allow(unused)]
-use std::io::{stdin, stdout, BufRead, BufWriter, StdinLock, StdoutLock, Write};
+macro_rules! some_or {
+    ($e:expr, $err:expr) => {{
+        match $e {
+            Some(r) => r,
+            None => $err,
+        }
+    }};
+}
 
 fn main() {
-    let mut io = IO::stdio();
+    let mut input = In::stdin();
 }
 
-/// Modified EbTech's Scanner
-pub struct IO<R, W: Write> {
-    reader: R,
-    writer: BufWriter<W>,
-    buf_str: String,
-    buf_iter: std::str::SplitWhitespace<'static>,
-}
+struct In(std::str::SplitAsciiWhitespace<'static>, String);
 
-impl IO<StdinLock<'static>, StdoutLock<'static>> {
-    pub fn stdio() -> Self {
-        Self::new(stdin().lock(), stdout().lock())
-    }
-}
-
-impl<R: BufRead, W: Write> IO<R, W> {
-    pub fn new(reader: R, writer: W) -> Self {
-        Self {
-            reader,
-            writer: BufWriter::new(writer),
-            buf_str: String::new(),
-            buf_iter: "".split_whitespace(),
-        }
+impl In {
+    fn stdin() -> Self {
+        let buf = std::io::read_to_string(std::io::stdin().lock()).unwrap();
+        let iter = unsafe { std::mem::transmute(buf.trim().split_ascii_whitespace()) };
+        Self(iter, buf)
     }
 
-    #[inline]
-    pub fn next<T: std::str::FromStr>(&mut self) -> T {
-        loop {
-            if let Some(token) = self.buf_iter.next() {
-                return token.parse().ok().expect("Failed parse");
-            }
-            self.buf_str.clear();
-            self.reader
-                .read_line(&mut self.buf_str)
-                .expect("Failed read");
-            self.buf_iter = unsafe { std::mem::transmute(self.buf_str.split_whitespace()) };
-        }
+    fn read<T: std::str::FromStr + std::fmt::Debug>(&mut self) -> T {
+        self.0.next().and_then(|s| s.parse::<T>().ok()).unwrap()
     }
-}
-
-macro_rules! io_shortcut {(
-    $(($type:ident, $types:ident))*
-) => (
-    $(
-        impl<R: BufRead, W: Write> IO<R, W> {
-            #[inline]
-            #[allow(unused)]
-            fn $type(&mut self) -> $type {
-                self.next()
-            }
-            #[inline]
-            #[allow(unused)]
-            fn $types(&mut self, n: usize) -> Vec<$type> {
-                (0..n).map(|_| self.next()).collect()
-            }
-        }
-    )*
-)}
-
-io_shortcut! {
-    (u8, u8s) (u16, u16s) (u32, u32s) (u64, u64s) (u128, u128s)
-    (i8, i8s) (i16, i16s) (i32, i32s) (i64, i64s) (i128, i128s)
-    (usize, usizes)
 }

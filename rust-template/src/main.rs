7884bb50a48b9d8f80a153db7ce3f94c054a15f7
Jeonghyeon Kim <powergee101@gmail.com>
2024-05-18 15:38:03 +0900

    Update the Rust template

--- rust-template/src/main.rs
+++ rust-template/src/main.rs
@@ -1,19 +1,23 @@
 #![allow(unused)]
+use std::io::{read_to_string, stdin, stdout, BufWriter, Write};
+use std::str::{FromStr, SplitAsciiWhitespace};
 
 fn main() {
     let mut input = In::stdin();
+    let mut writer = BufWriter::new(stdout().lock());
 }
 
-struct In(std::str::SplitAsciiWhitespace<'static>, String);
+struct In(SplitAsciiWhitespace<'static>);
 
 impl In {
     fn stdin() -> Self {
-        let buf = std::io::read_to_string(std::io::stdin().lock()).unwrap();
-        let iter = unsafe { std::mem::transmute(buf.trim().split_ascii_whitespace()) };
-        Self(iter, buf)
+        let iter = read_to_string(stdin().lock())
+            .map(|buf| buf.leak().trim().split_ascii_whitespace())
+            .unwrap();
+        Self(iter)
     }
 
-    fn read<T: std::str::FromStr + std::fmt::Debug>(&mut self) -> T {
+    fn read<T: FromStr>(&mut self) -> T {
         self.0.next().and_then(|s| s.parse::<T>().ok()).unwrap()
     }
 }
